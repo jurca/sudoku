dc3d5c83e16cb7d4d2a0befefc839f002c3edca8
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-12-19 11:01:09 +0100

    allowed modifications of already filled-in values

--- ui/Cell.ts
+++ ui/Cell.ts
@@ -9,16 +9,22 @@ export default function Cell(
   onShowValuePicker: () => void,
   onCellValueToggled: (cell: ISudokuMatrixCell, toggledValue: number) => void,
 ) {
+  const expandedNotes = [...new Array(9)].map((_, index) => cell.userMarkedOptions.includes(index + 1))
+
   return html`
     <sudoku-cell class=${styles.cell}>
-      ${cell.value ?
-        cell.value
+      ${cell.initialValue ?
+        cell.initialValue
       :
         html`
-          <button class=${styles.cellButton} onclick=${onShowValuePicker}></button>
+          <button class=${styles.cellButton} onclick=${onShowValuePicker}>${cell.value ?
+            cell.value
+          :
+            ''
+          }</button>
           ${hasValuePicker ?
             ValuePicker(
-              [...new Array(9)].map((_, index) => cell.userMarkedOptions.includes(index + 1)),
+              expandedNotes,
               onCellValueToggled.bind(null, cell),
             )
           :
