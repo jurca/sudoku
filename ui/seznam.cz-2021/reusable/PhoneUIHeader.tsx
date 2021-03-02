10edff974404520cee171434b0ebe8755b54b23a
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-03-02 09:24:05 +0100

    Add the PhoneUIComponent

---
+++ ui/seznam.cz-2021/reusable/PhoneUIHeader.tsx
@@ -0,0 +1,31 @@
+import classnames from 'classnames'
+import * as React from 'react'
+import Icon, {IconType} from './Icon'
+import IconButton from './IconButton'
+import isAndroidOrIOS from './isAndroidOrIOS'
+import styles from './phoneUIHeader.css'
+
+interface IProps {
+  readonly children: string
+  readonly onGearAction?: () => void
+}
+
+export default function PhoneUIHeader(props: IProps) {
+  if (!isAndroidOrIOS()) {
+    return null
+  }
+
+  const isIOS = / iPhone /.test(navigator.userAgent)
+
+  return (
+    <div className={classnames(styles.header, isIOS && styles.ios)}>
+      <a href="menu.html" className={styles.buttonWrapper}>
+        <Icon icon={IconType.ARROW_LEFT}/>
+      </a>
+      <div className={styles.title}>{props.children}</div>
+      <div className={styles.buttonWrapper}>
+        <IconButton icon={IconType.GEAR} onAction={props.onGearAction}/>
+      </div>
+    </div>
+  )
+}
