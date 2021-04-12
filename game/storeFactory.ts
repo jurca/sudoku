a05627b4af3003e6cdc4d4a8f35a890b2d8a4cfd
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-04-12 13:21:56 +0200

    Integrate support for redux dev tools

--- game/storeFactory.ts
+++ game/storeFactory.ts
@@ -1,6 +1,14 @@
-import {createStore} from 'redux'
+import {createStore, StoreEnhancer} from 'redux'
 import {ActionType} from './Action'
 import reducer from './reducer'
 import {DEFAULT_STATE, IState} from './state'
 
-export default () => createStore<IState, ActionType, {}, {}>(reducer, DEFAULT_STATE)
+declare global {
+  // tslint:disable-next-line:interface-name
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: () => StoreEnhancer<{}, {}>
+  }
+}
+
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__
+export default () => createStore<IState, ActionType, {}, {}>(reducer, DEFAULT_STATE, reduxDevTools && reduxDevTools())
--- ui/seznam.cz-2021/storeFactory.ts
+++ ui/seznam.cz-2021/storeFactory.ts
@@ -3,4 +3,5 @@ import { ActionType } from './Action'
 import reducer from './reducer'
 import {DEFAULT_STATE, IState} from './state'
 
-export default () => createStore<IState, ActionType, {}, {}>(reducer, DEFAULT_STATE)
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__
+export default () => createStore<IState, ActionType, {}, {}>(reducer, DEFAULT_STATE, reduxDevTools && reduxDevTools())
