2a075e636e6e3e5e90055635848e85cd1b8ffe93
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-04-12 13:18:29 +0200

    Create application-specific wrapper of the GameBoard
    This component implements the lower-level of application logic and
    provides some further abstraction.

---
+++ ui/seznam.cz-2021/app/GameBoard.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react'
+import {createHierarchicalCellMatrix} from '../../../game/selectors'
+import {ISudokuMatrixCell, SudokuMatrix} from '../../../game/state'
+import GameBoardUI from '../blocks/GameBoard'
+import {InputMode} from '../blocks/InputModeSwitch'
+import GameBoardTheme from '../theme/GameBoardTheme'
+import PrimaryColor from '../theme/PrimaryColor'
+import {NOTED_VALUES_ATTRIBUTE, SELECTED_VALUE_ATTRIBUTE} from '../theme/stateReflectingAttributes'
+import Theme from '../theme/Theme'
+
+interface IProps {
+  readonly gameState: SudokuMatrix
+  readonly inputModeSwitchName: string
+  readonly defaultInputMode: InputMode
+  readonly uniqueClassName: string
+  readonly primaryColor: PrimaryColor
+  readonly theme: Theme
+  onToggleCellValue(cell: ISudokuMatrixCell, value: null | number, mode: InputMode): void
+}
+
+export default function GameBoard(props: IProps) {
+  const [selectedCell, setSelectedCell] = React.useState<null | {readonly row: number, readonly column: number}>(null)
+  const [inputMode, setInputMode] = React.useState(props.defaultInputMode)
+
+  const selectedCellObject = selectedCell && props.gameState[selectedCell.row][selectedCell.column]
+
+  const onCellAction = React.useMemo(
+    () => (cell: ISudokuMatrixCell) => {
+      if (cell.initialValue) {
+        return
+      }
+
+      if (inputMode === InputMode.ERASE) {
+        props.onToggleCellValue(cell, null, inputMode)
+        return
+      }
+
+      const row = props.gameState.findIndex((matrixRow) => matrixRow.includes(cell))
+      const column = row > -1 ? props.gameState[row].findIndex((otherCell) => otherCell === cell) : -1
+      setSelectedCell({row, column})
+    },
+    [props.gameState, inputMode, setSelectedCell, props.onToggleCellValue],
+  )
+  const onInputModeChange = React.useMemo(
+    () => (newInputMode: InputMode) => {
+      if (newInputMode === InputMode.ERASE) {
+        setSelectedCell(null)
+      }
+      setInputMode(newInputMode)
+    },
+    [setSelectedCell, setInputMode],
+  )
+  const onInput = React.useMemo(
+    () => (pressedKey: number) => {
+      if (inputMode === InputMode.ERASE || !selectedCellObject) {
+        return
+      }
+      if (inputMode === InputMode.NOTES && selectedCellObject.value) {
+        return
+      }
+
+      props.onToggleCellValue(selectedCellObject, pressedKey, inputMode)
+    },
+    [selectedCellObject, inputMode],
+  )
+
+  const stateAttributes = {
+    ...(selectedCellObject && selectedCellObject.value && {
+      [SELECTED_VALUE_ATTRIBUTE]: selectedCellObject.value,
+    }),
+    ...(selectedCellObject && !selectedCellObject.value && selectedCellObject.userMarkedOptions.length && {
+      [NOTED_VALUES_ATTRIBUTE]: selectedCellObject.userMarkedOptions.join(' '),
+    }),
+  }
+
+  return (
+    <div className={props.uniqueClassName}>
+      <div {...stateAttributes}>
+        <GameBoardUI
+          gameState={createHierarchicalCellMatrix(props.gameState)}
+          selectedCell={selectedCellObject}
+          defaultInputMode={inputMode}
+          inputModeSwitchName={props.inputModeSwitchName}
+          onInputModeChange={onInputModeChange}
+          onCellAction={onCellAction}
+          onInput={onInput}
+        />
+        <GameBoardTheme
+          selectorPrefix={`.${props.uniqueClassName} `}
+          primaryColor={props.primaryColor}
+          theme={props.theme}
+        />
+      </div>
+    </div>
+  )
+}
