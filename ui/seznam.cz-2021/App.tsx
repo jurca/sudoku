43876ddeaebde60571c0dde7ad92f581157c266e
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-04-15 08:14:19 +0200

    Integrate the PhoneUIHeader

--- ui/seznam.cz-2021/App.tsx
+++ ui/seznam.cz-2021/App.tsx
@@ -1,18 +1,45 @@
 import * as React from 'react'
+import {connect} from 'react-redux'
+import {showDialog} from './Action'
 import style from './app.css'
 import Main from './app/Main'
 import {InputMode} from './blocks/InputModeSwitch'
+import Dialog from './dialog/Dialog'
 import DialogHost from './dialog/DialogHost'
+import PhoneUIHeader from './reusable/PhoneUIHeader'
+import {IState} from './state'
 
-export default function App() {
+interface ICallbackProps {
+  onOpenSettings(): void
+}
+
+type Props = ICallbackProps
+
+function App(props: Props) {
   return (
     <div className={style.app}>
-      <Main
-        inputModeSwitchName="inputMode"
-        defaultInputMode={InputMode.INPUT}
-        themeStyleNameSpacingClassName="main"
-      />
+      <div className={style.phoneHeader}>
+        <PhoneUIHeader onGearAction={props.onOpenSettings}>
+          Sudoku
+        </PhoneUIHeader>
+      </div>
+      <div className={style.main}>
+        <Main
+          inputModeSwitchName="inputMode"
+          defaultInputMode={InputMode.INPUT}
+          themeStyleNameSpacingClassName="main"
+        />
+      </div>
       <DialogHost/>
     </div>
   )
 }
+
+export default connect<{}, ICallbackProps, {}, IState>(
+  null,
+  {
+    onOpenSettings: showDialog.bind(null, {dialog: Dialog.SETTINGS, stack: false}),
+  },
+)(
+  App,
+)
--- ui/seznam.cz-2021/app.css.d.ts
+++ ui/seznam.cz-2021/app.css.d.ts
@@ -2,6 +2,8 @@
 // Please do not change this file!
 interface CssExports {
   'app': string;
+  'main': string;
+  'phoneHeader': string;
 }
 export const cssExports: CssExports;
 export default cssExports;
