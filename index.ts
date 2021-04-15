520f9306351d7d987c3344c99d4dd971919fd875
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-04-15 09:29:48 +0200

    Move the UI container's ID to a constant

--- index.ts
+++ index.ts
@@ -4,8 +4,10 @@ import {Provider} from 'react-redux'
 import App from './ui/seznam.cz-2021/App'
 import storeFactory from './ui/seznam.cz-2021/storeFactory'
 
+const UI_CONTAINER_ID = 'app'
+
 addEventListener('DOMContentLoaded', () => {
-  const appRoot = document.getElementById('app')
+  const appRoot = document.getElementById(UI_CONTAINER_ID)
   if (!appRoot) {
     throw new Error('Cannot find the app container element')
   }
