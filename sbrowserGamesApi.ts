a4d6ab44a29f0764a47cea83df34d46f627c309b
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-05-17 14:00:31 +0200

    Add adapter for native SBrowser API bindings

---
+++ sbrowserGamesApi.ts
@@ -0,0 +1,125 @@
+declare global {
+    interface Window {
+        sbrowser?: {[key: string]: unknown}
+        webkit?: {[key: string]: unknown}
+    }
+
+    var sbrowser: unknown
+    var webkit: unknown
+}
+
+interface ApiBindings {
+    [key: string]: unknown
+}
+
+const sbrowserAndroidApiBindings: ApiBindings = typeof sbrowser === 'object' && sbrowser ? sbrowser as ApiBindings : {}
+const sbrowserIOSApiBindings: ApiBindings = (typeof webkit === 'object' && webkit && (webkit as any).messageHandlers ?
+    (webkit as any).messageHandlers
+:
+    {}
+)
+
+export function isTablet(): boolean {
+    if (typeof navigator === 'object' && navigator && typeof navigator.userAgent === 'string') {
+        if (/\(iPad;/.test(navigator.userAgent)) {
+            return true
+        }
+    }
+
+    if (typeof sbrowserAndroidApiBindings.isTablet === 'function') {
+        try {
+            return sbrowserAndroidApiBindings.isTablet()
+        } catch (sbrowserApiError) {
+            console.error('SBrowser API.isTablet: failed to execute the isTabled method', sbrowserApiError)
+            return false
+        }
+    }
+
+    console.warn('SBrowser API.isTablet: no supported SBrowser native API is available, defaulting to false')
+    return false
+}
+
+export function terminateApp(): void {
+    callNativeVoidReturningMethod('terminateApp', [], ['terminate'])
+}
+
+export function gamesPlay(gameId: string): void {
+    callNativeVoidReturningMethod('gamesPlay', [gameId])
+}
+
+export function gamesExit(gameId: string, gamesPlayed: number, gamesWon: number): void {
+    callNativeVoidReturningMethod('gamesExit', [gameId, gamesPlayed, gamesWon], [JSON.stringify({
+        game: gameId,
+        "games-played": gamesPlayed,
+        "games-won": gamesWon,
+    })])
+}
+
+export function openLoginForm(): void {
+    callNativeVoidReturningMethod('openLoginForm')
+}
+
+export function isSignedIn(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        if (typeof sbrowserAndroidApiBindings.isSignedIn === 'function') {
+            try {
+                const result = sbrowserAndroidApiBindings.isSignedIn()
+                resolve(result)
+                return
+            } catch (sbrowserApiError) {
+                console.error('SBrowser API.isSignedIn: Failed to execute the isSignedIn method', sbrowserApiError)
+                reject(sbrowserApiError)
+                return
+            }
+        }
+
+        if (
+            sbrowserIOSApiBindings.isSignedIn &&
+            (sbrowserIOSApiBindings.isSignedIn as any).postMessage === 'function'
+        ) {
+            try {
+                window.sbrowser = window.sbrowser || {}
+                window.sbrowser.isSignedIn = (isUserSignedIn: boolean) => {
+                    resolve(!!isUserSignedIn)
+                    if (window.sbrowser && window.sbrowser.isSignedIn) {
+                        delete window.sbrowser.isSignedIn
+                    }
+                }
+                ;(sbrowserIOSApiBindings.isSignedIn as any).postMessage()
+                return
+            } catch (sbrowserApiError) {
+                console.error('SBrowser API.isSignedIn: Failed to execute the isSignedIn method', sbrowserApiError)
+                reject(sbrowserApiError)
+                return
+            }
+        }
+
+        reject(new Error('No supported native SBrowser API is available'))
+    })
+}
+
+function callNativeVoidReturningMethod(
+    methodName: string,
+    androidArguments: readonly unknown[] = [],
+    iosArguments: readonly unknown[] = androidArguments,
+): boolean {
+    if (typeof sbrowserAndroidApiBindings[methodName] === 'function') {
+        try {
+            (sbrowserAndroidApiBindings[methodName] as Function)(...androidArguments)
+            return true
+        } catch (sbrowserApiError) {
+            console.error(`SBrowser API.${methodName}: Failed to execute the ${methodName} method`, sbrowserApiError)
+        }
+    }
+
+    if (sbrowserIOSApiBindings[methodName] && (sbrowserIOSApiBindings[methodName] as any).postMessage === 'function') {
+        try {
+            (sbrowserIOSApiBindings[methodName] as any).postMessage(...iosArguments)
+            return true
+        } catch (sbrowserApiError) {
+            console.error(`SBrowser API.${methodName}: Failed to execute the ${methodName} method`, sbrowserApiError)
+        }
+    }
+
+    return false
+}
