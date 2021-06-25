fa4e01219a3522edad11b7babee760b06727b262
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-06-25 11:47:03 +0200

    Fix code style

--- ui/seznam.cz-2021/storeFactory.ts
+++ ui/seznam.cz-2021/storeFactory.ts
@@ -1,5 +1,5 @@
 import {createStore} from 'redux'
-import { ActionType } from './Action'
+import {ActionType} from './Action'
 import reducer from './reducer'
 import {DEFAULT_STATE, IState} from './state'
 
