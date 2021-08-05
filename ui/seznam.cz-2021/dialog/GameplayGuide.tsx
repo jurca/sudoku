2cb9bad9fddf4203880616ed694551dd6f54e332
Martin Jurča <martin.jurca@firma.seznam.cz>
2021-08-05 12:10:01 +0200

    Add the text of the gameplay guide

--- ui/seznam.cz-2021/dialog/GameplayGuide.tsx
+++ ui/seznam.cz-2021/dialog/GameplayGuide.tsx
@@ -17,30 +17,46 @@ function GameplayGuide(props: Props) {
 
   return (
     <article className={styles.guide}>
-      <h1>Gameplay guide</h1>
+      <p><strong>
+        Cílem hry je doplnit do herní mřížky čísla 1&ndash;9 tak, aby každé číslo bylo v&nbsp;každém řádku,
+        v&nbsp;každém sloupci a v&nbsp;každém z devíti čtverců, ze kterých je herní mřížka složena, právě jednou.
+      </strong></p>
+
+      <h1>Zápis čísel do mřížky</h1> 
+
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Integer vitae justo eget magna fermentum iaculis. Libero nunc consequat interdum varius sit amet
-        mattis. Euismod in pellentesque massa placerat duis ultricies lacus. Rutrum quisque non tellus orci ac auctor
-        augue mauris. Eu scelerisque felis imperdiet proin fermentum leo vel orci porta. Facilisis magna etiam tempor
-        orci eu lobortis elementum. Lobortis elementum nibh tellus molestie nunc. Consectetur lorem donec massa sapien.
-        Pharetra vel turpis nunc eget lorem dolor. Adipiscing commodo elit at imperdiet dui. Rutrum quisque non tellus
-        orci ac auctor augue mauris. Eleifend mi in nulla posuere sollicitudin. At ultrices mi tempus imperdiet nulla
-        malesuada pellentesque elit. Varius morbi enim nunc faucibus a pellentesque sit amet. Id nibh tortor id aliquet
-        lectus proin nibh nisl condimentum. Sem et tortor consequat id porta nibh venenatis cras sed.
+        Pro zadání čísla je potřeba označit buňku, do které chcete číslo vložit, a zvolit příslušnou číslici
+        v&nbsp;řadě pod herní mřížkou. Při hře na počítači lze využít také fyzickou klávesnici.
       </p>
-      <h2>About rules</h2>
-      <h3>Rules, as they are</h3>
       <p>
-        Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Nunc sed blandit libero volutpat sed cras.
-        Porta non pulvinar neque laoreet suspendisse. Amet massa vitae tortor condimentum lacinia quis. Nec sagittis
-        aliquam malesuada bibendum arcu vitae elementum. Sed augue lacus viverra vitae congue eu consequat ac felis.
-        Ornare arcu dui vivamus arcu felis bibendum ut. Ornare lectus sit amet est placerat in egestas. Pellentesque
-        elit eget gravida cum sociis natoque penatibus et magnis. Tincidunt praesent semper feugiat nibh sed pulvinar
-        proin gravida. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Amet venenatis urna cursus
-        eget nunc. Arcu bibendum at varius vel pharetra. Vulputate mi sit amet mauris commodo quis imperdiet massa
-        tincidunt. Lectus magna fringilla urna porttitor rhoncus dolor purus non enim. Neque aliquam vestibulum morbi
-        blandit cursus risus at ultrices mi.
+        Čísla lze zadávat ve dvou různých módech, mezi kterými se lze přepínat pomocí ikon pod číselnou řadou.
+      </p>
+
+      <h2>Zápis kandidátů</h2>
+
+      <p>
+        Pokud pro určitou buňku existuje právě jedno možné číslo, které do ní lze doplnit, aby splňovalo výše popsaná
+        pravidla, zapisuje se jako tzv. kandidát. Mód pro zápis kandidátů se aktivuje pomocí první ikony (označené
+        číslicí „1“) v&nbsp;trojici ikon pro přepínání módů. V&nbsp;nastavení hry lze zvolit, zda se má správnost
+        kandidátů kontrolovat automaticky. Pokud je tato kontrola aktivní, do mřížky nelze zapsat číslo, které už se
+        v&nbsp;příslušném řádku, sloupci či čtverci jednou nachází. 
+      </p>
+
+      <h2>Zápis poznámek</h2>
+
+      <p>
+        Mód poznámek slouží k&nbsp;zaznamenání více možných kandidátů v&nbsp;případě, kdy zatím nelze pro danou buňku
+        jednoznačně určit finálního kandidáta. Do každé buňky lze vepsat až devět různých hodnot. Mód pro zápis
+        poznámek se aktivuje pomocí prostřední ikony v&nbsp;trojici ikon pro přepínání módů. V&nbsp;nastavení hry lze
+        zvolit, zda se mají poznámky automaticky aktualizovat na základě kandidátů vyplněných do ostatních buněk.
+      </p>
+
+      <h2>Mazání kandidátů a poznámek</h2>
+
+      <p>
+        Zapsané kandidáty i poznámky lze mazat přepnutím do módu pro mazání, který se aktivuje pomocí třetí ikony
+        (označené obrázkem gumy) v&nbsp;trojici ikon pro přepínání módů, případně označením příslušné buňky a opětovným
+        zvolením číslice, která má být smazána.
       </p>
     </article>
   )
